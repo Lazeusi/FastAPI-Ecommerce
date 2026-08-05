39656b66172d72f3807353927dcb76fc85b0e08e
Lazeusi <lazeusi@gmail.com>
2026-08-05 19:14:02 +0330

    update

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-def main():
-    print("Hello from fastapi-ecommerce!")
+from fastapi import FastAPI
+import uvicorn
 
+app = FastAPI(
+    title="FastAPI E-Commerce API",
+    version="1.0.0",
+)
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to the FastAPI E-Commerce API!"}
 
 if __name__ == "__main__":
-    main()
+
+    uvicorn.run("main:app", host="127.0.0.1", port=8000 ,reload=True)
